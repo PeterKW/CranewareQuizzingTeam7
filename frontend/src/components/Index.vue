a6fa5916f176dc92ab3fa30efa3d539e0dfeebbd
Daniel <dkblackley@dundee.ac.uk>
2020-10-07 17:58:30 +0100

    updated button UI
    Made it more flashy

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -17,15 +17,17 @@
               maxlength="8"
             />
           </b-form-group>
-          <b-button @click="join('single')" variant="primary" class="btn-block">
+          <div class="btn-group d-flex" role="group">
+          <b-button @click="join('single')" variant="primary" class="button_base b09_electric">
             Single Player
           </b-button>
-          <b-button variant="primary" class="btn-block">
+          <b-button variant="primary" class="button_base b09_electric">
             Join Lobby
           </b-button>
-          <b-button variant="primary" class="btn-block">
+          <b-button variant="primary" class="button_base b09_electric" >
             Create Lobby
           </b-button>
+        </div>
         </b-form>
       </b-row>
     </b-container>
@@ -44,6 +46,52 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.b09_electric {
+    color: #000000;
+    background-color: #ffffff;
+    border: #000000 solid 1px;
+    padding: 10px;
+    transition: all 0.1s ease;
+    -webkit-transition: all 0.1s ease;
+    -moz-transition: all 0.1s ease;
+}
+
+.b09_electric:hover {
+    color: #ffffff;
+    background-color: #000000;
+    animation: b09_electric_blinkIn 0.1s step-end 0 2;
+    -webkit-animation: b09_electric_blinkIn 0.1s step-end 0 2;
+    -moz-animation: b09_electric_blinkIn 0.1s step-end 0 2;
+    transition: all 0.2s ease 0.2s;
+    -webkit-transition: all 0.2s ease 0.2s;
+    -moz-transition: all 0.2s ease 0.2s;
+}
+
+@-webkit-keyframes b09_electric_blinkIn {
+    from,
+    to {
+        background-color: #f8f8f8;
+        color: #080808;
+    }
+    50% {
+        background-color: #ffffff;
+        color: #000000;
+    }
+}
+
+@-moz-keyframes b09_electric_blinkIn {
+    from,
+    to {
+        background-color: #f8f8f8;
+        color: #080808;
+    }
+    50% {
+        background-color: #ffffff;
+        color: #000000;
+    }
+}
+
 
 h1 {
   text-align: center;
