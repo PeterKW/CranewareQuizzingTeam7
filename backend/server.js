b0020a4efcd08077225eacf01ce98de83da50a32
ajholligan <33227000+ajholligan@users.noreply.github.com>
2020-10-07 11:58:00 +0100

    Added reponse to home page and 404 error

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const app = express();
+const ports = 3000
 
-app.listen(3000)
+app.listen(ports)
 
+//Responds to request for test page
 app.get('/test', (req, res) => {
-
+    //TODO get actual webpage to respond
     res.send('<p>hello team 7</p>');
+})
+
+//Responds to request for home page
+app.get('/home', (req, res) => {
+    //TODO get actual webpage to respond
+    res.send('<p>Homepage</p>');
+})
+
+//Responds 404 if page requested is not known
+app.use((req, res) => {
+    res.status(404).send('<h2>404 Page Not Found</h2>');
 })
