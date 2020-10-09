2bb0d335e3688be95be37d2808979750f01eed2b
adidude <menon.or.adithya@gmail.com>
2020-10-09 05:38:15 +0300

    Added functionality for event communication between the server and client

--- backend/applet.js
+++ backend/applet.js
@@ -1,22 +1,29 @@
 // Import Pusher dependancy
 const Pusher = require('pusher');
-// const express = require('express');
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const crypto = require('crypto');
 
 // Logs all changes to connections to console
 Pusher.logToConsole = true;
 
-/*
-// Servers as handler for routing
+// Serves as handler for routing
 const webapp = express();
 
+// Using bodyparser to enforce json rules
+// Refer to https://www.npmjs.com/package/body-parser
+webapp.use(bodyparser.json());
+webapp.use(bodyparser.urlencoded({ extended: false }));
+
+// Enable CORS to prevent errors when accessing
+webapp.use(cors());
+
+// Index API route for the Express app
 webapp.get('/', function(req, res)
 {
-
-})
-*/
-
-// Listening to port 5000
-// webapp.listen(5000);
+	res.send('Welcome');
+});
 
 // Instantiates the connection to the Pusher API
 const pusher = new Pusher({
@@ -24,22 +31,30 @@ const pusher = new Pusher({
 	key: '072127b07acd646fc5ec',
 	secret: '48f388a42b3a7991bbc5',
 	cluster: 'eu',
-	encrypted: true,
+	useTLS: true,
 });
 
-/* NOT FUNCTIONAL
-// Subscribes to the test channel
-const evntReader = pusher.subscribe('test');
-*/
+// Authenticates users
+webapp.post('/pusher/auth', function(req, res)
+{
+	const presence = { user_id: crypto.randomBytes(16).toString('hex') };
+	const socketId = req.body.socket_id;
+	const channel = req.body.channel_name;
+	const auth = pusher.authenticate(socketId, channel, presence);
+	res.send(auth);
+});
+
+// Recieve data from clients
+webapp.post('/hook', (req, res) =>
+{
+	console.log(req.body);
+	res.sendStatus(200);
+});
 
 // Triggers an event
-pusher.trigger('test', 'my-event', {
+pusher.trigger('private-lobby', 'my-event', {
 	'message': 'hello world',
 });
 
-/* NOT FUNCTIONAL
-evntReader.bind('my-event', function(data)
-{
-	console.log('successful event recieved with ' + data.message);
-});
-*/
+// Listening to port 5000
+webapp.listen(5000);
